611029c4524d8aa9eb8a235365b71c68c1f709a5
karim <karim@karim-PC.mshome.net>
2018-09-28 19:29:11 +0200

    Création projet SpringBoot

---
+++ boutiqueEnligne/src/main/java/com/boutiqueEnLigne/BoutiqueEnligneApplication.java
@@ -0,0 +1,12 @@
+package com.boutiqueEnLigne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueEnligneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiqueEnligneApplication.class, args);
+	}
+}
